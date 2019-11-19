11cfef580accd87f2fcb0039baf4bc830eb2974a
Alethea Cary <aletheacary@gmail.com>
2019-11-19 06:35:09 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -41,7 +41,7 @@ class Application
 
   def handle_add(item)
     if @@items.include?(item)
-      @@cart<<item
+      @@cart.push(item)
       @@items.delete_at(@@items.index(item))
       return "added #{item}"
     else
